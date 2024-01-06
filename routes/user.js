bd83bbbf7cada9741a38728d40ebe9bc1476f1c7
Jeremy Lakey <70728274+JeremyLakey@users.noreply.github.com>
2024-01-05 21:25:04 -0700

    Implement Salt and Hashing

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,32 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const UserModel = require('../model/schema/userSchema')
 const userRoutes = express.Router()
+const salty = require('../utils/salty')
 
 userRoutes.post('/', bodyParser.json(), async (req, res) => {
     console.log(req.body)
     if(req.body) {
-        let user = await UserModel.create({name:req.body.name, password: req.body.password})
-        //await UserModel.updateOne(user)
-        res.send({id:req.body.id, name:req.body.name, password: req.body.password})
+        let user = await UserModel.create({name:req.body.name, password: await salty.saltPassword(req.body.password)})
+        res.send(user)
+    }
+    else {
+        res.status(400)
+        res.send({err:"oof"});
+    }
+    
+})
+
+userRoutes.post('/login', bodyParser.json(), async (req, res) => {
+    console.log(req.body)
+    if(req.body) {
+        let user = await UserModel.findOne({name:req.body.name})
+        if (await salty.validateUser(req.body.password, user.password)) {
+            res.send(user)
+        }
+        else {
+            res.status(403)
+            res.send({id:req.body.id, name:req.body.name})
+        }
     }
     else {
         res.status(400)
---
+++ utils/salty.js
@@ -0,0 +1,25 @@
+
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+
+const saltPassword = async (password) => {
+    return await bcrypt.hash(password, saltRounds).then((hash) => {return hash}).catch((err) => console.error(err))
+}
+
+const validateUser = (password, hash) => {
+    bcrypt
+      .compare(password, hash)
+      .then(result => {
+        return result
+      })
+      .catch(err => {
+        console.error(err.message)
+         return false
+        })        
+}
+
+module.exports = {
+    saltPassword: saltPassword,
+    validateUser: validateUser
+}
+
