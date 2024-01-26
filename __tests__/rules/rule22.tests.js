ddccf059b76fe9006f5d4fd8bd12cc120d733a6b
Jeremy Lakey <70728274+JeremyLakey@users.noreply.github.com>
2024-01-25 23:15:42 -0700

    test 21 and 22

--- __tests__/rules/rule22.tests.js
+++ __tests__/rules/rule22.tests.js
@@ -1,51 +1,59 @@
 
 const rules = require("../../model/rules/rule")
-const rule = rules[1]
+const rule = rules[21]
 
-// "A < 3 : A = 3 : A > 3"
+// "Total < 6 : Total = 6 : Total > 6"
 
-describe("Rule 0", () => {
-    test('Works as intended for A = 1', () => {
+describe("Rule 21", () => {
+    test('Works as intended for Total < 6', () => {
         rule.setMode(0)
-        for(let i = 0; i < 100; i++) {
-            let count = 0
-            let compare = 0
-            for(let a = 1; a <= 5; a++) {
-                for(let b = 1; b <= 5; b++) {
-                    for(let c = 1; c <=5 ; c++) {
-                        if(rule.checkRule(a,b,c)) count++
-                        if(a === 1) compare++
-                    }
-                }
-            }
-            expect(count).toBe(25)
-            expect(count).toBe(compare)
-        }
-        
-    })
-
-    test('Works as intended for A > 1', () => {
-        rule.setMode(1)
-        for(let i = 0; i < 100; i++) {
-            let count = 0
-            let compare = 0
-            for(let a = 1; a <= 5; a++) {
-                for(let b = 1; b <= 5; b++) {
-                    for(let c = 1; c <=5 ; c++) {
-                        if(rule.checkRule(a,b,c)) count++
-                        if(a > 1) compare++
-                    }
-                }
-            }
-            expect(count).toBe(100)
-            expect(count).toBe(compare)
-        }
-        
-    })
-
-    test('Implement Me', () => {
         let count = 0
-        expect(count).toBe(1)
+        let compare = 0
+        for(let a = 1; a <= 5; a++) {
+            for(let b = 1; b <= 5; b++) {
+                for(let c = 1; c <=5 ; c++) {
+                    if(rule.checkRule(a,b,c)) count++
+                    if((a + b + c) < 6) compare++
+                }
+            }
+        }
+        expect(count).toBe(100)
+        expect(count).toBe(compare)
+        
+    })
+
+    test('Works as intended for Total = 6', () => {
+        rule.setMode(1)
+        let count = 0
+        let compare = 0
+        for(let a = 1; a <= 5; a++) {
+            for(let b = 1; b <= 5; b++) {
+                for(let c = 1; c <=5 ; c++) {
+                    if(rule.checkRule(a,b,c)) count++
+                    if((a + b + c) === 6) compare++
+                }
+            }
+        }
+        expect(count).toBe(10)
+        expect(count).toBe(compare)
+        
+    })
+
+    test('Works as intended for Total > 6', () => {
+        rule.setMode(2)
+        let count = 0
+        let compare = 0
+        for(let a = 1; a <= 5; a++) {
+            for(let b = 1; b <= 5; b++) {
+                for(let c = 1; c <=5 ; c++) {
+                    if(rule.checkRule(a,b,c)) count++
+                    if((a + b + c) === 6) compare++
+                }
+            }
+        }
+        expect(count).toBe(105)
+        expect(count).toBe(compare)
+        
     })
 } 
 )
